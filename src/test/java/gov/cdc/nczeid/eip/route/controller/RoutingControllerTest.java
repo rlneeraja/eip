525db1b0154628378a17a5037f45b21facf893b1
Marcelo Caldas <mcq1@cdc.gov>
2017-10-27 09:35:58 -0400

    merging conflicts

--- src/test/java/gov/cdc/nczeid/eip/route/controller/RoutingControllerTest.java
+++ src/test/java/gov/cdc/nczeid/eip/route/controller/RoutingControllerTest.java
@@ -1,38 +1,21 @@
 package gov.cdc.nczeid.eip.route.controller;
 
 import gov.cdc.nczeid.eip.route.model.Route;
-import gov.cdc.nczeid.eip.route.services.RoutingService;
-import gov.cdc.nczeid.eip.uilt.LoadJson;
+import gov.cdc.nczeid.eip.utils.LoadJson;
+import io.restassured.response.Response;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-
-<<<<<<< HEAD
-import static io.restassured.RestAssured.given;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-=======
-import gov.cdc.nczeid.eip.route.model.Route;
-import gov.cdc.nczeid.eip.route.services.RoutingService;
-import gov.cdc.nczeid.eip.utils.LoadJson;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
-import static org.junit.Assert.*;
-
-import java.util.List;
 
 @RunWith(SpringRunner.class)
->>>>>>> neeraja
 @TestPropertySource(locations="classpath:application-test.yml")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT) 
 public class RoutingControllerTest {
@@ -40,25 +23,15 @@ public class RoutingControllerTest {
     private static final String APPLICATION_JSON = "application/json";
     @Value("${serverRootURL}")
     private String serverURL ;
-
+    private String rootAPIIURL;
     private String routeEndpoint;
 
-<<<<<<< HEAD
-    @Autowired
-    private RoutingService service;
 
-
-    @Before
-    public void setup() throws Exception {
-        this.routeEndpoint = serverURL + "/routing-services/v1/";
-=======
-    
     @Before
     public void setup() throws Exception {
         this.rootAPIIURL = serverURL + "/routing-services/v1/route";
         this.routeEndpoint = rootAPIIURL;
         System.out.println(routeEndpoint);
->>>>>>> neeraja
     }
 
     @After
