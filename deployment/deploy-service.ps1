c1c1631ce974293ea6d3c49d8451ad12984b3754
mkrystof <ygj6@cdc.gov>
2017-10-27 09:09:57 -0500

    Fixed deployment scripts

--- deployment/deploy-service.ps1
+++ deployment/deploy-service.ps1
@@ -1,18 +1,30 @@
-Copy-Item -Path ../target/EIPRoutingService-*.jar -Destination \\EIP-QA\services\routing-services.jar
+param(
+[string]$deploy_hostname
+)
 
-Copy-Item -Path ./winsw-2.1.2-bin.exe -Destination \\EIP-QA\services\routing-services.exe
+Write-Host Deploying to environment hostname `"$deploy_hostname`"...
 
-Copy-Item -Path ./routing-services.xml -Destination \\EIP-QA\services\routing-services.xml
+Invoke-Command -ComputerName $deploy_hostname -ScriptBlock {
+    if (Get-Service routing-services -ErrorAction SilentlyContinue)
+    {
+       E:\services\routing-services.exe stop
+    }
+}
+# Wait for the service to stop if it was running.  This has to be outside of the script block above.
+Start-Sleep -m 1000
 
-Invoke-Command -ComputerName EIP-QA -ScriptBlock {
+Copy-Item -Path ../target/EIPRoutingService-*.jar -Destination \\$deploy_hostname\services\routing-services.jar
+
+Copy-Item -Path ./winsw-2.1.2-bin.exe -Destination \\$deploy_hostname\services\routing-services.exe
+
+Copy-Item -Path ./routing-services.xml -Destination \\$deploy_hostname\services\routing-services.xml
+
+Invoke-Command -ComputerName $deploy_hostname -ScriptBlock {
     if (-NOT (Get-Service routing-services -ErrorAction SilentlyContinue))
     {
         E:\services\routing-services.exe install
-        
-        E:\services\routing-services.exe start
-    }
-    else
-    {
-        E:\services\routing-services.exe restart
-    }
+        sleep 2
+    } 
+    
+    E:\services\routing-services.exe start
 }
