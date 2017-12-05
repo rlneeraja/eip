3721f8b942a8dec40ab19086178c6f79460b8a75
nLakshmipathy <ide9@cdc.gov>
2017-12-04 22:25:35 -0500

    1. made Create and Update as seperate endpoints 2. Soft delete with version Number 3. Swagger doc fine tuned added annotations 4. CreateDate UpdateDates are auto filled on create and update

--- src/main/java/gov/cdc/nczeid/eip/route/controller/RoutingController.java
+++ src/main/java/gov/cdc/nczeid/eip/route/controller/RoutingController.java
@@ -1,19 +1,22 @@
 package gov.cdc.nczeid.eip.route.controller;
 
-
-
 import gov.cdc.nczeid.eip.rest.ApiVersion;
 import gov.cdc.nczeid.eip.route.model.ERROR_CODES;
 import gov.cdc.nczeid.eip.route.model.ErrorResponse;
 import gov.cdc.nczeid.eip.route.model.Route;
+import gov.cdc.nczeid.eip.route.model._SWRoute;
 import gov.cdc.nczeid.eip.route.services.EventSender;
 import gov.cdc.nczeid.eip.route.services.RouteNotFoundException;
 import gov.cdc.nczeid.eip.route.services.RoutingService;
-import gov.cdc.nczeid.eip.utils.InputStreamUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -23,13 +26,9 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.util.ContentCachingRequestWrapper;
-
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
@@ -43,6 +42,12 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 public class RoutingController {
 
     private Log log = LogFactory.getLog(RoutingController.class);
+    
+    @Value("${default.createdBy}")
+    private String createdBy;
+    
+    @Value("${default.updatedBy}")
+    private String updatedBy;
   
     @Autowired
     private RoutingService routingService;
@@ -50,70 +55,174 @@ public class RoutingController {
     @Autowired
     private EventSender eventSender;
     
+    @ApiOperation(value = "Routes Message with guid to for a specific rabitmq queue for condition code", 
+    		notes = "Routes Message with guid to for a specific rabitmq queue for condition code", 
+    		response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Routing successfully done"),
+            @ApiResponse(code = 207, message = "Routing partially done with multi status"),
+            @ApiResponse(code = 400, message = "Any error that is caused by the user - bad data sent, business rules violated, etc"),
+            @ApiResponse(code = 403, message = "Caller is not authorized to execute this method"),
+            @ApiResponse(code = 500, message = "Any Server side error can be reported via a 500. those errors are not recoverable by they user - something is wrong with the system")
+    	}
+    ) 
     @RequestMapping(value = "/routeMessage", method = POST)
 	public ResponseEntity routeMessage(@Valid @RequestParam String condition, @RequestParam String mguid,
 			HttpServletRequest request) throws Exception {
 		String routedTo = "";
 		// get the condition and queue map
 		Iterable<Route> routes = routingService.getRoutesByCondition(condition);
-		if (routes != null) {
+		long size = routes.spliterator().getExactSizeIfKnown();
+		String respMessage = "";
+		boolean atLeastOneSuccess = false;
+		
+		if (size > 0 ) {
 			Iterator<Route> itRoutes = routes.iterator();
 			while (itRoutes.hasNext()) {
 				Route r = itRoutes.next();
-				if (r.getDestination() != null && !r.getDestination().isEmpty())
-					eventSender.send(condition, mguid);
+				if (r.getDestination() != null && !r.getDestination().isEmpty()){
+				//	try{
+						eventSender.send(r.getDestination(), mguid);
+				//		atLeastOneSuccess = true;
+						respMessage += "Message with guid " + mguid + " is sucessfully routed to queue " + r.getDestination() + "\n";
+				//	}
+//					catch(Exception e)
+//					{
+//						respMessage += "Message with guid " + mguid + " failed to route to " + r.getDestination() + "\n";
+//					}
+				}
 			}
+//			if(!atLeastOneSuccess)
+//				throw new Exception();
 		} else {
 			ErrorResponse error = new ErrorResponse( ERROR_CODES.UNPROCESSABLE_ENTITY,
 					"No Routes defined for condition. Please try again later or contact EIP's system administrator to get the route created for condition code",
-					request.getRequestURL().toString(), 422, "");
-			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
+					request.getRequestURL().toString(), 400, "");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 		}
-		return ResponseEntity.status(HttpStatus.ACCEPTED)
-				.body("Message with guid " + mguid + " is routed to queue " + routedTo);
+		
+//		if(size >0)
+//			return ResponseEntity.status(HttpStatus.MULTI_STATUS)
+//				.body(respMessage);
+//		else
+//			return ResponseEntity.status(HttpStatus.OK)
+//					.body(respMessage);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(respMessage);
 	}
 
+    @ApiOperation(value = "Get Route resource for a soecific routeId", 
+    		notes = "Get Route resource for a soecific routeId", 
+    		response = _SWRoute.class, produces="application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Registration successfully created"),
+            @ApiResponse(code = 400, message = "Any error that is caused by the user - bad data sent, business rules violated, etc"),
+            @ApiResponse(code = 403, message = "Caller is not authorized to execute this method"),
+            @ApiResponse(code = 409, message = "Conflict"),
+            @ApiResponse(code = 500, message = "Any Server side error can be reported via a 500. those errors are not recoverable by they user - something is wrong with the system")
+    	}
+    )
     @RequestMapping(value="/route/{routeId}", method = GET)
     public ResponseEntity getRoute(@PathVariable UUID routeId,HttpServletRequest request) throws RouteNotFoundException, Exception{
         Route route = routingService.findByRouteId(routeId);
             return ResponseEntity.ok(route);
     }
     
+    @ApiOperation(value = "Get all Route resources", 
+    		notes = "Get all Route resources", 
+    		response = _SWRoute.class, produces="application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Registration successfully created"),
+            @ApiResponse(code = 400, message = "Any error that is caused by the user - bad data sent, business rules violated, etc"),
+            @ApiResponse(code = 403, message = "Caller is not authorized to execute this method"),
+            @ApiResponse(code = 409, message = "Conflict"),
+            @ApiResponse(code = 500, message = "Any Server side error can be reported via a 500. those errors are not recoverable by they user - something is wrong with the system")
+    	}
+    )
     @RequestMapping(value="/routes", method = GET)
     public ResponseEntity getRoutes(HttpServletRequest request) throws  Exception {
         Iterable<Route> routes = routingService.getAll();
         return ResponseEntity.ok(routes);
     }
 
+    @ApiOperation(value = "Create a Route resources", 
+    		notes = "Create a Route resources", 
+    		response = _SWRoute.class, produces="application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Route successfully created"),
+            @ApiResponse(code = 400, message = "Any error that is caused by the user - bad data sent, business rules violated, etc"),
+            @ApiResponse(code = 403, message = "Caller is not authorized to execute this method"),
+            @ApiResponse(code = 409, message = "Conflict"),
+            @ApiResponse(code = 500, message = "Any Server side error can be reported via a 500. those errors are not recoverable by they user - something is wrong with the system")
+    	}
+    )
     @RequestMapping(value = "/route", method = POST)
-    public ResponseEntity save(@Valid @RequestBody Route route, HttpServletRequest request)  throws  Exception{
-    	if(route.getVersion() == null && route.getRouteId() != null){
-    		ErrorResponse error = new ErrorResponse( ERROR_CODES.CONFLICT, "Route can be updated on an existing version", request.getRequestURL().toString() , 409, "");
-       	    return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    public ResponseEntity create(@Valid @RequestBody Route route, HttpServletRequest request)  throws  Exception{
+    	Iterable<Route> existingRoute = routingService.findByConditionAndDestination(route.getCondition(), route.getDestination());
+    	long size = existingRoute.spliterator().getExactSizeIfKnown();
+    	if(route.getRouteId() == null && size == 0 ){
+    		Route r = routingService.save(route);
+	       	return ResponseEntity.status(HttpStatus.OK).body(r);
     	}
     	else{
-       	 	Route r = routingService.save(route);
-	        return ResponseEntity.status(HttpStatus.OK).body(r);
+    		ErrorResponse error = new ErrorResponse( ERROR_CODES.BAD_REQUEST, "Route already exist and can not be created", request.getRequestURL().toString() , 409, "");
+       	    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     	}
     		
     }
     
-    @RequestMapping(value = "/route/{routeId}", method = PUT)
-    public ResponseEntity delete(@PathVariable UUID routeId, HttpServletRequest request) throws RouteNotFoundException, Exception {
-    		Route route = routingService.findByRouteId(routeId);
+    @ApiOperation(value = "Update a Route resources", 
+    		notes = "Update a Route resources", 
+    		response = _SWRoute.class, produces="application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Route successfully updated"),
+            @ApiResponse(code = 400, message = "Any error that is caused by the user - bad data sent, business rules violated, etc"),
+            @ApiResponse(code = 403, message = "Caller is not authorized to execute this method"),
+            @ApiResponse(code = 409, message = "Conflict"),
+            @ApiResponse(code = 500, message = "Any Server side error can be reported via a 500. those errors are not recoverable by they user - something is wrong with the system")
+    	}
+    )
+    @RequestMapping(value = "/route", method = PUT)
+    public ResponseEntity update(@Valid @RequestBody Route route, HttpServletRequest request)  throws  Exception{
+	       	 	Route r = routingService.save(route);
+		        return ResponseEntity.status(HttpStatus.OK).body(r);
     		
-    		if(route.getActive() == 1){
-    			route.setActive(0);
-	    		route = routingService.save(route);
-	        	return ResponseEntity.status(HttpStatus.OK).body(route);
-        	}
-	    	else{
-	    		ErrorResponse error = new ErrorResponse( ERROR_CODES.CONFLICT, "Route is already inactive", request.getRequestURL().toString() , 409, "");
-	       	    return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
-	        }
     }
     
-  
+    @ApiOperation(value = "Delete a Route resource", 
+    		notes = "Delete a Route resource", 
+    		response = _SWRoute.class, produces="application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Route successfully deleted"),
+            @ApiResponse(code = 400, message = "Any error that is caused by the user - bad data sent, business rules violated, etc"),
+            @ApiResponse(code = 403, message = "Caller is not authorized to execute this method"),
+            @ApiResponse(code = 409, message = "Conflict"),
+            @ApiResponse(code = 500, message = "Any Server side error can be reported via a 500. those errors are not recoverable by they user - something is wrong with the system")
+    	}
+    )
+    @RequestMapping(value = "/route/{routeId},{userSuppliedVersion}", method = PUT)
+    public ResponseEntity delete(@PathVariable UUID routeId, @PathVariable Long userSuppliedVersion,HttpServletRequest request) throws RouteNotFoundException, Exception {
+    		Route route = routingService.findByRouteId(routeId);
+    		if(route.getVersion() == userSuppliedVersion){
+	    		if(route.getActive() == 1){
+	    			route.setActive(0);
+		    		route = routingService.save(route);
+		        	return ResponseEntity.status(HttpStatus.OK).body(route);
+	        	}
+		    	else{
+		    		ErrorResponse error = new ErrorResponse( ERROR_CODES.CONFLICT, "Route is already marked deleted", request.getRequestURL().toString() , 409, "");
+		       	    return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+		        }
+    		}
+    		else{
+    			ErrorResponse error = new ErrorResponse( ERROR_CODES.CONFLICT, "Version mismatch on the route, Please refresh and try again.", request.getRequestURL().toString() , 409, "");
+	       	    return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    		}
+    			
+    		
+    }
+    
+    @ApiOperation(hidden=true, value = "Exception Handling")
     @ExceptionHandler({HttpMessageNotReadableException.class, HttpMediaTypeNotSupportedException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
@@ -123,6 +232,7 @@ public class RoutingController {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
+    @ApiOperation(hidden=true, value = "Exception Handling")
     @ExceptionHandler({MethodArgumentTypeMismatchException.class, RouteNotFoundException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @RequestMapping("/notfound")
@@ -131,6 +241,7 @@ public class RoutingController {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
     
+    @ApiOperation(hidden=true, value = "Exception Handling")
     @ExceptionHandler({Exception.class })
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @RequestMapping("/serverError")
@@ -139,6 +250,7 @@ public class RoutingController {
 	    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
     
+    @ApiOperation(hidden=true, value = "Exception Handling")
     @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ResponseBody
---
+++ src/test/java/gov/cdc/nczeid/eip/route/controller/GenerateSwaggerYamlTest.java
@@ -0,0 +1,64 @@
+package gov.cdc.nczeid.eip.route.controller;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.google.common.base.Charsets;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@ActiveProfiles("dev")
+@TestPropertySource(locations="classpath:application.yml")
+public class GenerateSwaggerYamlTest {
+    @Value("${server.apiContext}")
+    private String apiContext;
+    
+    @Value("${server.context-path}")
+    private String contextPath;
+
+    
+    @Value("${server.versionContext}")
+    private String versionContext;
+    
+    @Value("${server.port}")
+    private String port;
+    
+    private String rootAPIIURL;
+
+    @Before
+    public void setUp() throws Exception {
+        this.rootAPIIURL = "http://localhost:10001"+
+        		contextPath+"/"+apiContext+ "/"+  versionContext+"/api-docs";
+    }
+	@Test
+	public void generate() throws IOException{
+		InputStream in = new URL(rootAPIIURL).openStream();
+		String jsonString = IOUtils.toString(in, Charsets.UTF_8);    
+	    JsonNode jsonTree = new ObjectMapper().readTree(jsonString);
+	    String yaml = new YAMLMapper().writeValueAsString(jsonTree);
+	    
+	    FileWriter writer = new FileWriter("./src/docs/swaggerTest.yaml");
+	    writer.write(yaml);
+	    writer.close();
+	}
+}
